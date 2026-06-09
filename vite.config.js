8b9150dd8131229e03adf64b23c1ba08f1255569
Janine Laping <jdlaping@gmail.com>
2026-06-09 16:01:22 +0800

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/ThunderQuizMasterReact/',
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-    },
-  },
-});
+  base: '/ThunderQuizMasterReact/'
+})
